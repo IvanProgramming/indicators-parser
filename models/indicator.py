e30aa3216a6c6324b55026706a64b6da07923f7b
Ivan <iyuvlasov_1@edu.hse.ru>
2022-12-07 17:18:23 +0300

    More models added

---
+++ models/indicator.py
@@ -0,0 +1,61 @@
+from enum import Enum
+from typing import Optional
+from uuid import UUID
+
+from pydantic import BaseModel
+from tortoise import Model, fields
+
+from models.report import ReportPD
+from models.users import UserPD
+
+
+class IndicatorGroup(Model):
+    """ ORM Model of Indicators Group """
+    id = fields.UUIDField(pk=True)
+    description = fields.TextField(null=True)
+    owner = fields.ForeignKeyField('User')
+
+
+class IndicatorType(Enum):
+    """ Enum type for Indicator """
+    HASH = "HASH"
+    DOMAIN = "DOMAIN"
+    IP_ADDRESS = "IP_ADDRESS"
+
+
+class Indicator(Model):
+    """ ORM Model of Indicators """
+    id = fields.UUIDField(pk=True)
+    type = fields.CharEnumField(IndicatorType)
+    value = fields.TextField()
+    owner = fields.ForeignKeyField('User', on_delete=fields.SET_NULL)
+    group = fields.ForeignKeyField('IndicatorGroup', on_delete=fields.SET_NULL)
+    report = fields.ForeignKeyField('Report', on_delete=fields.SET_NULL)
+
+
+class IndicatorGroupPD(BaseModel):
+    """ Pydantic model of Indicator Group, might be used in http responses """
+
+    id: UUID
+    """ UUID-typed id of group """
+    description: Optional[str]
+    """ Optional description of group """
+    owner: Optional[UserPD]
+    """ Group owner """
+
+
+class IndicatorPD(BaseModel):
+    """ Pydantic model of Indicator, might be used in http responses """
+
+    id: UUID
+    """ ID of Indicator """
+    type: IndicatorType
+    """ Type of indicator, like HASH or DOMAIN  """
+    value: str
+    """ Raw value of indicator """
+    owner: Optional[UserPD]
+    """ Owner of indicator, on delete becomes NULL """
+    group: Optional[IndicatorGroupPD]
+    """ Group, that has this indicator as its member, NULL on unset """
+    report: Optional[ReportPD]
+    """ Report, that was parsed to get this indicator, NULL on unset """
---
+++ models/report.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from uuid import UUID
+
+from pydantic import BaseModel
+from tortoise import Model, fields
+
+from models.users import UserPD
+
+
+class Report(Model):
+    """ ORM Model of Loaded for parsing report """
+    id = fields.UUIDField(pk=True)
+    owner = fields.ForeignKeyField('User', on_delete=fields.CASCADE)
+    mime = fields.CharField(max_length=64)
+    loaded_at = fields.DatetimeField(auto_now=True)
+    is_public = fields.BooleanField(default=False)
+
+
+class ReportPD(BaseModel):
+    """ Pydantic model of report, might be used in http responses """
+
+    id: UUID
+    """ ID of Indicator """
+    owner: UserPD
+    """ User, that loaded this report """
+    mime: str
+    """ Mime type of report """
+    loaded_at: datetime
+    """ Date and time of loading report """
+    is_public: bool
+    """ Flag, shows, might this report and all indicators from it become public """
+
+    class Config:
+        orm_mode = True
+        fields = {
+            "is_public": {"exclude": True}
+        }
