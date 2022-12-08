0965aee71e9d0f9afae688c5fcb0ac85385a9302
Ivan <iyuvlasov_1@edu.hse.ru>
2022-12-08 17:53:36 +0300

    Docstring added

--- auth/utils.py
+++ auth/utils.py
@@ -2,7 +2,12 @@ from jwcrypto.jwk import JWK
 
 
 def create_and_save_key(key_file_name="key.jwk"):
-    """ Generates new RSA key and stores it in file """
+    """
+        Generates new RSA key and stores it in file
+
+    Args:
+        key_file_name (str): save path for jwk key
+    """
     key = JWK.generate(kty="RSA", size=2048)
     with open(key_file_name, "w+") as f:
         f.write(key.export())
