10f60066edfb730740cdcbb94dae20fce0485436
Ivan <iyuvlasov_1@edu.hse.ru>
2022-12-11 23:06:46 +0300

    Removed unused print

--- endpoints/user.py
+++ endpoints/user.py
@@ -8,6 +8,5 @@ from responses.responses import OkResponse
 async def get_me(request: Request):
     """ Returns user itself serialized """
     user: User = request.state.user
-    print(user)
     user_model = UserPD.from_orm(user)
     return OkResponse(user_model)
